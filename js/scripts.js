93ebc08935d32ce35cf7ec75b0ceac82d85dd6fb
Николай <nicky.kravtsov@gmail.com>
2018-12-28 16:51:53 +0300

    decor

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-var link = document.querySelector(".categories__list-link");
-var popup = document.querySelector(".categories__list-wrapper");
-
+var link = document.querySelector(".catalog__item-js");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal__close");
 
 
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add(".categories__modal-show");
+  popup.classList.add("modal-show");
 });
 
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains(".categories__modal-show")) {
-      popup.classList.remove(".categories__modal-show");
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
     }
   }
 });
